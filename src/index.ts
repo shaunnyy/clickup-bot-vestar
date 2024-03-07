aaf0d009861fade15663b206d4ea9de18c48f466
trendyjosh <trendyjosh@gmail.com>
2024-03-07 17:05:00 +0000

    Set axios defaults from config

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,14 @@ import { SapphireClient, container } from "@sapphire/framework";
 import { GatewayIntentBits } from "discord.js";
 import config from "./config.json";
 import { ClickUpController } from "./controllers/ClickUpController";
+import axios from "axios";
 
-// Create the music player object
+// Create the clickup object
 container.controller = new ClickUpController();
+
+// Set axios defaults
+axios.defaults.headers.common["Authorization"] = config.clickupToken;
+axios.defaults.baseURL = config.clickupEndpoint;
 
 // Initiate the Sapphire client
 const client = new SapphireClient({
